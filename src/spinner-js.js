e355f8d010a8f5abaa750a268413483289a0f9cf
Faizan <mdfaizan7563@gmail.com>
2022-09-06 00:01:13 +0530

    Work

---
+++ src/spinner-js.js
@@ -0,0 +1,20 @@
+function loader (url, loaded) {
+
+  
+   var   loading = document.getElementById("loading");
+
+  loading.classList.add("load");
+
+  fetch(url)
+  .then(res => res.text())
+  .then(txt => {
+    main.innerHTML = txt;
+    if (typeof loaded == "function") { loaded(); }
+  })
+  .finally(() => {
+    
+    window.setTimeout(()=>{
+      loading.classList.remove("load");
+    }, 2000);
+  });
+}
